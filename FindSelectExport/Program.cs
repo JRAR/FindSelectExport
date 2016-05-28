4638e595987f306a9526cd3324223b3dd1f979bf
Jesse Hemphill <gamewinner1728@gmail.com>
2016-05-27 23:07:15 -0400

    Added GUI Design; Barebones functionality

--- FindSelectExport/Program.cs
+++ FindSelectExport/Program.cs
@@ -15,7 +15,7 @@ namespace FindSelectExport
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmFSE());
         }
     }
 }
